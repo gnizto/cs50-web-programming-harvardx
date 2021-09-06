00eb2c33eb8a170acfae7d6e146c848f82f30aa9
unknown <gabrieldm@tutanota.de>
2021-09-05 22:57:01 -0300

    Add Lecture2 and Start Project0

---
+++ lecture2/sequences.py
@@ -0,0 +1,13 @@
+# name = "Gabriel"
+
+# print(name[1])
+
+# names = ["Harry", "Ron", "Hermione"]
+
+# print(names[0])
+
+# Tuples can't be changed
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
